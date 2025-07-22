42c1dc972aba7f9f943c7258d4d0584e5f042517
Sahil Gawade <104153594+Sahil-2005@users.noreply.github.com>
2025-07-22 17:29:46 +0530

    Successfully shifted the DB from local SQLlite to xampp SQL

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,11 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 login_manager = LoginManager()
- 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secretkey123'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/tracker.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/expense_tracker'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
